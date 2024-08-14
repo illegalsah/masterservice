ed7bc695125252072072b4103631d5d19763a1f7
padmanabha <spadm22@gmail.com>
2024-08-14 21:02:07 +0530

    master setup

---
+++ src/main/java/io/pn/masterservice/MasterserviceApplication.java
@@ -0,0 +1,13 @@
+package io.pn.masterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterserviceApplication.class, args);
+	}
+
+}
